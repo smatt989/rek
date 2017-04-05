9e8c37e74bf644041ab7f13e13b8e0e1398dbc4a
matt <matthew.slotkin@gmail.com>
2017-04-05 00:01:18 -0700

    removed old dependency

--- src/main/scala/com/example/app/migrations/Migration.scala
+++ src/main/scala/com/example/app/migrations/Migration.scala
@@ -1,6 +1,6 @@
 package com.example.app.migrations
 
-import com.example.app.models.{Adventure, User}
+import com.example.app.models.{User}
 import com.example.app.{AppGlobals, Tables}
 import slick.driver.PostgresDriver.api._
 
