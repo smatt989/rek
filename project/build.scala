6edac5fa67342b70efb9ba92934e6753efd02964
matt <matthew.slotkin@gmail.com>
2017-04-26 17:53:25 -0700

    upping version

--- project/build.scala
+++ project/build.scala
@@ -10,7 +10,7 @@ import ScalateKeys._
 object TestappBuild extends Build {
   val Organization = "com.slotkin"
   val Name = "Rekki"
-  val Version = "0.2.2-SNAPSHOT"
+  val Version = "0.2.3-SNAPSHOT"
   val ScalaVersion = "2.11.8"
   val ScalatraVersion = "2.5.0"
 
